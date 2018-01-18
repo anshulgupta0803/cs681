08937f526bdce1da779ba6cc44f8811f34550055
Anshul Gupta <anshulgupta0803@gmail.com>
2018-01-18 17:09:21 +0530

    Scripts to perform load test using httperf and collecting utilization from server logs

---
+++ assignment1/scripts/client/loadtest_httperf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Usage: ./loadtest_httperf.sh ARRIVAL_RATE"
+	exit
+fi
+
+SERVER="bernoulli"
+SCRIPT="/loop.php"
+ARRIVAL_RATE=$1 # requests / sec
+DURATION=60 # seconds
+NUM_CONNS=$(expr $ARRIVAL_RATE \* $DURATION)
+
+rm -f "Client_$ARRIVAL_RATE.log" 2> /dev/null
+echo "Arrival Rate: $ARRIVAL_RATE"
+echo $ARRIVAL_RATE | nc $SERVER 7777
+httperf --hog --server $SERVER --uri $SCRIPT --timeout 100 --num-conns $NUM_CONNS --num-calls 1 --rate $ARRIVAL_RATE > "Client_$ARRIVAL_RATE.log"
+nc $SERVER 8888
+
---
+++ assignment1/scripts/server/utilization_httperf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Usage: ./utilization.sh ARRIVAL_RATE"
+	exit
+fi
+
+ARRIVAL_RATE=$1
+LOG_FILE="Server_$ARRIVAL_RATE.log"
+
+UTILIZATION=`awk '
+		BEGIN {
+			n = 0
+			idle_time = 0
+		}
+		{
+			if ($1 != "procs" && $1 != "r") {
+				idle_time += $15
+				n++
+			}
+		}
+		END {
+			print (100 - (idle_time / n)) / 100
+		}
+	' $LOG_FILE`
+echo $ARRIVAL_RATE,$UTILIZATION
+

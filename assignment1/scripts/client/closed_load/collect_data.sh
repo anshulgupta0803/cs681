b83e4c27bfb577f020bf1356d2658c98d0569755
Anshul Gupta <anshulgupta0803@gmail.com>
2018-01-25 00:04:16 +0530

    Calculating fraction of dropped requests

--- assignment1/scripts/client/closed_load/collect_data.sh
+++ assignment1/scripts/client/closed_load/collect_data.sh
@@ -10,7 +10,6 @@ GENERATE_DATA=0
 if [ "$2" == "true" ]; then
 	GENERATE_DATA=1
 fi
-CWD=$(pwd)
 
 cd Client_$ARRIVAL_RATE/logs/*
 
@@ -20,8 +19,15 @@ fi
 SESSION_LINE_NO=$(grep -n "session" report.html | cut -d':' -f1)
 THROUGHPUT_LINE_NO=$(expr $SESSION_LINE_NO + 4)
 RESPONSE_TIME_LINE_NO=$(expr $SESSION_LINE_NO + 5)
+FINISH_USERS_COUNT_LINE_NO=$(expr $(grep -n "finish_users_count" report.html | cut -d':' -f1) + 1)
+RESPONSE_200_COUNT_LINE_NO=$(expr $(grep -n "200</td>" report.html | cut -d':' -f1) + 3)
+
 THROUGHPUT=$(cat report.html | sed -n $THROUGHPUT_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
 RESPONSE_TIME=$(cat report.html | sed -n $RESPONSE_TIME_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
-echo -e "$ARRIVAL_RATE\t$THROUGHPUT\t$RESPONSE_TIME"
-cd $CWD
+
+FINISH_USERS_COUNT=$(cat report.html | sed -n $FINISH_USERS_COUNT_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
+RESPONSE_200_COUNT=$(cat report.html | sed -n $RESPONSE_200_COUNT_LINE_NO's|<td class="stats">||p' | sed 's|</td>||g' |awk '{print $1}')
+FRACTION_DROPPED_REQUESTS=$(echo "($FINISH_USERS_COUNT - $RESPONSE_200_COUNT) / $FINISH_USERS_COUNT" | bc -l)
+
+echo -e "$ARRIVAL_RATE\t$THROUGHPUT\t$RESPONSE_TIME\t$FRACTION_DROPPED_REQUESTS"
 

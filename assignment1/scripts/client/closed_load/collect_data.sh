272daf02e4fcdacda811aa703bc71a39cd9b732d
Anshul Gupta <anshulgupta0803@gmail.com>
2018-01-30 00:24:28 +0530

    Client logs of runs with and without dvfs

--- assignment1/scripts/client/closed_load/collect_data.sh
+++ assignment1/scripts/client/closed_load/collect_data.sh
@@ -16,18 +16,18 @@ cd Client_$ARRIVAL_RATE/logs/*
 if [ $GENERATE_DATA -eq 1 ]; then
 	/usr/lib/tsung/bin/tsung_stats.pl
 fi
-SESSION_LINE_NO=$(grep -n "session" report.html | cut -d':' -f1)
+SESSION_LINE_NO=$(grep -n "request" report.html | cut -d':' -f1)
 THROUGHPUT_LINE_NO=$(expr $SESSION_LINE_NO + 4)
 RESPONSE_TIME_LINE_NO=$(expr $SESSION_LINE_NO + 5)
-FINISH_USERS_COUNT_LINE_NO=$(expr $(grep -n "finish_users_count" report.html | cut -d':' -f1) + 1)
-RESPONSE_200_COUNT_LINE_NO=$(expr $(grep -n "200</td>" report.html | cut -d':' -f1) + 3)
+#FINISH_USERS_COUNT_LINE_NO=$(expr $(grep -n "finish_users_count" report.html | cut -d':' -f1) + 1)
+#RESPONSE_200_COUNT_LINE_NO=$(expr $(grep -n "200</td>" report.html | cut -d':' -f1) + 3)
 
 THROUGHPUT=$(cat report.html | sed -n $THROUGHPUT_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
 RESPONSE_TIME=$(cat report.html | sed -n $RESPONSE_TIME_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
 
-FINISH_USERS_COUNT=$(cat report.html | sed -n $FINISH_USERS_COUNT_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
-RESPONSE_200_COUNT=$(cat report.html | sed -n $RESPONSE_200_COUNT_LINE_NO's|<td class="stats">||p' | sed 's|</td>||g' |awk '{print $1}')
-FRACTION_DROPPED_REQUESTS=$(echo "($FINISH_USERS_COUNT - $RESPONSE_200_COUNT) / $FINISH_USERS_COUNT" | bc -l)
+#FINISH_USERS_COUNT=$(cat report.html | sed -n $FINISH_USERS_COUNT_LINE_NO's|<td class="stats">||p' | awk '{print $1}')
+#RESPONSE_200_COUNT=$(cat report.html | sed -n $RESPONSE_200_COUNT_LINE_NO's|<td class="stats">||p' | sed 's|</td>||g' |awk '{print $1}')
+#FRACTION_DROPPED_REQUESTS=$(echo "($FINISH_USERS_COUNT - $RESPONSE_200_COUNT) / $FINISH_USERS_COUNT" | bc -l)
 
-echo -e "$ARRIVAL_RATE\t$THROUGHPUT\t$RESPONSE_TIME\t$FRACTION_DROPPED_REQUESTS"
+echo -e "$ARRIVAL_RATE\t$THROUGHPUT\t$RESPONSE_TIME" #\t$FRACTION_DROPPED_REQUESTS"
 
